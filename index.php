ee22dfe8cf1f4875567744ed107b3dca5ad9cb99
Steven Arif <stevenarif123@gmail.com>
2024-03-11 00:58:11 +0800

    menambah index.php menjadi untuk halaman utama

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman Utama</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <h1>Selamat Datang di Website Kami</h1>
+    <nav>
+      <ul>
+        <li><a href="about.php">Tentang Kami</a></li>
+        <li><a href="products.php">Produk</a></li>
+        <li><a href="services.php">Layanan</a></li>
+        <li><a href="contact.php">Kontak</a></li>
+        <li><a href="login.php" class="btn-login">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section>
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt tincidunt augue, a tristique lorem commodo eget. Donec eleifend sit amet augue sit amet condimentum. Mauris consectetur consequat interdum. Suspendisse eget orci eu ipsum accumsan fermentum. Quisque sed augue ut nisl volutpat tincidunt. In hac habitasse platea dictumst. Donec aliquet ornare nisl, non efficitur metus consectetur in. Proin ac vehicula turpis.</p>
+    </section>
+
+    <section>
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt tincidunt augue, a tristique lorem commodo eget. Donec eleifend sit amet augue sit amet condimentum. Mauris consectetur consequat interdum. Suspendisse eget orci eu ipsum accumsan fermentum. Quisque sed augue ut nisl volutpat tincidunt. In hac habitasse platea dictumst. Donec aliquet ornare nisl, non efficitur metus consectetur in. Proin ac vehicula turpis.</p>
+    </section>
+  </main>
+
+  <footer>
+    <p>Copyright © 2023</p>
+  </footer>
+</body>
+
+</html>
